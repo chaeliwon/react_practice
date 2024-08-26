7443e500ede6764b3d76fa3d34a9c9b780ad621a
chaeliwon <danyub987@gmail.com>
2024-08-26 09:23:26 +0900

    08260923

--- react02/src/component/Myteam.jsx
+++ react02/src/component/Myteam.jsx
@@ -1,13 +1,37 @@
+// import React from 'react'
+
+// const Myteam = (props) => {
+//   return (
+//     // JSX에서 inline style을 적용할때는 *객체 형식*으로 적용해야한다!!!!
+//     <div style={{border:"1px solid", textAlign:"center"}}>
+//         <p>
+//             {props.department}
+//             <br />
+//             {props.name}
+//         </p>
+//     </div>
+//   )
+// }
+
+// // defaultProps : 데이터를 전달받지 않을때 기본적으로 적용되는 데이터
+// Myteam.defaultProps = {
+//     name:"직원선별중"
+// }
+
+// export default Myteam
+
 import React from 'react'
 
-const Myteam = (props) => {
+const Myteam = ({department, name}) => {
+
+  console.log(department, name);
   return (
     // JSX에서 inline style을 적용할때는 *객체 형식*으로 적용해야한다!!!!
     <div style={{border:"1px solid", textAlign:"center"}}>
         <p>
-            {props.department}
+            {department}
             <br />
-            {props.name}
+            {name}
         </p>
     </div>
   )
