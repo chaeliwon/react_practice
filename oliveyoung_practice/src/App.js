80e2b14d9c7205d55b3125b5bc60899c3b94f697
chaeliwon <danyub987@gmail.com>
2024-08-28 09:02:35 +0900

    08280902

--- oliveyoung_practice/src/App.js
+++ oliveyoung_practice/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
+import MainPage from './page/MainPage';
+import LoginPage from './page/LoginPage';
+import ProductPage from './page/ProductDetails';
+import Header from './component/Header';
+
+/* 
+  실습1) 각 페이지 컴포넌트를 page폴더 내 생성
+  - 헤더영역 : Header.jsx
+  - 로그인페이지 : LoginPage.jsx
+  - 메인페이지 : MainPage.jsx
+  - 상품상세페이지 : ProductDetails.jsx
+
+  실습2) 페이지 컴포넌트를 화면에 출력할 수 있도록 라우터 설정
+  - 로그인: '/login'
+  - 메인: '/'
+  - 상품상세페이지: '/prdDetail'
+
+*/
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route path='/prdDetail' element={<ProductPage />} />
+      </Routes>
     </div>
   );
 }
