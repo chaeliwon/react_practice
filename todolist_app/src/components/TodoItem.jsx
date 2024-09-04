7fe5fe116bd0022292d2339c903021af639633c6
chaeliwon <danyub987@gmail.com>
2024-09-04 10:18:04 +0900

    09041018

--- todolist_app/src/components/TodoItem.jsx
+++ todolist_app/src/components/TodoItem.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-// import { FaCheckCircle } from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
 import { FaRegCircle } from "react-icons/fa";
 import { useDispatch } from 'react-redux';
-import { textChangeTodo } from '../redux/reducers/TodoSlice';
+import { checkChangeTodo, deleteTodo, textChangeTodo } from '../redux/reducers/TodoSlice';
 
 const TodoItem = ({ todo }) => {
 
@@ -13,42 +13,57 @@ const TodoItem = ({ todo }) => {
   // 초깃값 : 기존 등록한 내용 -> todo.text
   const [newText, setNewText] = useState(todo.text);
 
+  const [check, setCheck] = useState(todo.complete);
+
   const dispatch = useDispatch();
 
   const handleEdit = () => {
     setEdit(true)
   }
 
-  const handleSubmit =()=>{
+  const handleSubmit = () => {
     // store내 todoList 아이템 내용을 변경하기 위해서
     // 구분할 수 있는 id와 변경내용인 text를 객체형태로 전달
-    dispatch(textChangeTodo({id:todo.id, text : newText}))
+    dispatch(textChangeTodo({ id: todo.id, text: newText }))
     setEdit(false)
+  }
+
+  const handleChange = () => {
+    dispatch(checkChangeTodo({ id: todo.id }))
+    setCheck(false)
+  };
+
+  const handleDelete = () => {
+    dispatch(deleteTodo({ id: todo.id }))
   }
 
   return (
     <li className='todo-item'>
-      {/* <FaCheckCircle className='todo-item-checkbox' /> */}
-      <FaRegCircle className='todo-item-checkbox' style={{ color: 'lightgrey' }} />
+      {check ?
+        <FaCheckCircle className='todo-item-checkbox' onClick={handleChange} />
+        :
+        <FaRegCircle className='todo-item-checkbox' style={{ color: 'lightgrey' }} onClick={handleChange} />
+      }
+
+
 
       {edit ?
         <div>
-          <input 
-          type='text' 
-          className='todo-item-edit-input' 
-          value ={newText} 
-          onChange={(e)=>setNewText(e.target.value)}
+          <input
+            type='text'
+            className='todo-item-edit-input'
+            value={newText}
+            onChange={(e) => setNewText(e.target.value)}
           />
           <button className='todo-item-submit-btn' onClick={handleSubmit}>👍</button>
         </div>
         :
         <div>
-          <span className='todo-item-content'>{todo.text}</span>
-          <button className='todo-item-edit-btn' onClick={handleEdit}>✍</button>
+          <span className={`todo-item-content ${todo.complete ? 'todo-item-content-checked' : ''}`}>{todo.text}</span>
+          {/* {!todo.complete && <button className='todo-item-edit-btn' onClick={handleEdit}>✍</button>} */}
+          {todo.complete ? '' : <button className='todo-item-edit-btn' onClick={handleEdit}>✍</button>}
         </div>}
-
-
-      <button className='todo-item-delete-btn'>❌</button>
+      <button className='todo-item-delete-btn' onClick={handleDelete}>❌</button>
     </li>
   )
 }
--- todolist_app/src/redux/reducers/TodoSlice.js
+++ todolist_app/src/redux/reducers/TodoSlice.js
@@ -6,17 +6,25 @@ const TodoSlice = createSlice({
         todoList: []
     },
     reducers: {
-        addTodo: (state, action) => { 
+        addTodo: (state, action) => {
             state.todoList = state.todoList.concat(action.payload)
-         },
-        checkChangeTodo: () => { },
-        textChangeTodo: (state, action) => { 
-           state.todoList = state.todoList.map((item)=>({
+        },
+        checkChangeTodo: (state, action) => {
+            state.todoList = state.todoList.map((item) => ({
+                ...item,
+                complete: item.id === action.payload.id ? !item.complete : item.complete
+            }))
+        },
+        textChangeTodo: (state, action) => {
+            state.todoList = state.todoList.map((item) => ({
                 ...item,
                 text: item.id === action.payload.id ? action.payload.text : item.text
             }))
-         },
-        deleteTodo: () => { }
+        },
+        deleteTodo: (state, action) => {
+            //실습 ) todoList 내 삭제할 id를 비교하여 삭제기능 구현하기 (filter())
+            state.todoList = state.todoList.filter((item) => item.id !== action.payload.id);
+        }
     }
 })
 export const { addTodo, checkChangeTodo, textChangeTodo, deleteTodo } = TodoSlice.actions
