20edeafec8519f11f2e92b51f276cc2588b4e59f
chaeliwon <danyub987@gmail.com>
2024-09-20 14:02:37 +0900

    Update index.js

--- react08_Link/backend/routes/index.js
+++ react08_Link/backend/routes/index.js
@@ -8,25 +8,25 @@ router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'build', 'index.html'));
 })
 
-router.post('/getData', (req, res) => {
-    console.log('Form으로 요청이 들어옴!', req.body);
+// router.post('/getData', (req, res) => {
+//     console.log('Form으로 요청이 들어옴!', req.body);
 
-    let sql = 'INSERT INTO NODEJS_MEMBER VALUES(?,?,?)';
+//     let sql = 'INSERT INTO NODEJS_MEMBER VALUES(?,?,?)';
 
-    conn.query(sql, ['admin', 1234, req.body.data], (err, rows) => {
-        if (err) {
-            console.log('insert query 이슈 발생!');
-        }
-        if (rows) {
-            res.send({ result: "success" })
-        } else {
-            res.send({ result: "failed" })
-        }
-    })
+//     conn.query(sql, ['admin', 1234, req.body.data], (err, rows) => {
+//         if (err) {
+//             console.log('insert query 이슈 발생!');
+//         }
+//         if (rows) {
+//             res.send({ result: "success" })
+//         } else {
+//             res.send({ result: "failed" })
+//         }
+//     })
 
-    conn.end();
+//     conn.end();
 
-})
+// })
 
 //실습 ) 회원가입, 로그인 처리를 할 수 있는 기능 구현하기
 router.post("/join", (req, res) => {
